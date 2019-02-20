a6f468a68e5f24f3639a15463e6b1d68b7586ebd
lbrodeau <lbrodeau@login0.occigen>
2019-02-20 17:18:03 +0100

    LB

--- python/nemo_movie.py
+++ python/nemo_movie.py
@@ -303,8 +303,8 @@ if CNEMO == 'eNATL60':
         i2=8040; i1=i2-2560 ; j1=1525 ; j2=j1+1440 ; rfact_zoom=1. ; vcb=[0.025, 0.06, 0.4, 0.02] ; font_rat=2.*rfact_zoom
         l_annotate_name=False ; l_show_exp = True ; x_exp = 100 ; y_exp = 240
         x_clock = 200 ; y_clock = 170 ; x_logo = 1650 ; y_logo  = 1200
-        if CWHAT == 'SSS': tmin=33. ;  tmax=39.5   ;  df = 0.25 ; cb_jump = 2 ; cpal_fld = 'viridis' ;# cpal_fld = 'ncview_ssec'
-        if CWHAT == 'SST': tmin=10. ;  tmax=32.    ;  df = 1.   ; cb_jump = 1
+        if CWHAT == 'SSS': tmin=33. ;  tmax=39.5   ;  df = 0.25 ; cb_jump = 2 ; cpal_fld = 'magma' ;# cpal_fld = 'ncview_ssec'
+        if CWHAT == 'SST': tmin=10. ;  tmax=30.    ;  df = 1.   ; cb_jump = 1
 
     elif CBOX == 'Meddies':
         i2=5800; j1=1400; i1=i2-2560 ; j2=j1+1440 ; rfact_zoom=1. ; vcb=[0.5, 0.875, 0.485, 0.02] ; font_rat=2.*rfact_zoom
