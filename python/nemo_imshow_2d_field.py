507d2da14229d21d1e7c1cec5ef5ccbe7c3ff0f8
brodeau <brodeau@gmail.com>
2020-07-01 18:14:14 +0200

    LB

--- python/nemo_imshow_2d_field.py
+++ python/nemo_imshow_2d_field.py
@@ -26,6 +26,8 @@ import clprn_colmap as bcm
 
 import clprn_tool as bt
 
+bathy_max = 5000. # m
+
 color_top = 'white'
 clr_yellow = '#ffed00'
 
@@ -58,14 +60,16 @@ if narg ==6 :
     cf_lsm = sys.argv[5]
 
 
+l_bathy_var = [ 'Bathymetry', 'elevation' ]
+    
 
-if not l_read_lsm and cv_in != 'Bathymetry':
-    print "It's only for variable 'Bathymetry' that you can skip providing the mesh_mask file!"
+if not l_read_lsm and ( not cv_in in l_bathy_var):
+    print "It's only for bathymetric fields that you can skip providing the mesh_mask file!"
     sys.exit(0)
 
 
 
-bathy_max = 5000. # m
+
     
 i2=0
 j2=0
@@ -141,12 +145,24 @@ elif CNEMO == 'TROPICO2':
     x_cnf = 20. ; y_cnf = 3. ; # where to put label of conf on Figure...
     l_show_cb = True ; l_show_nm = True ; l_scientific_mode=False
     bathy_max = 6000. # m
+
+elif CNEMO == 'TROPICO05':
+    i1 = 0 ; j1 = 0 ; i2 = 0 ; j2 = 0 ; rfact_zoom = 3. ; vcb = [0.02, 0.15, 0.4, 0.04] ; font_rat = 2./rfact_zoom
+    x_cnf = 280. ; y_cnf = 135. ; # where to put label of conf on Figure...
+    l_show_cb = True ; l_show_nm = True ; l_scientific_mode=False
+    bathy_max = 6000. # m
     
 elif CNEMO == 'TROPICO12':
     i1 = 0 ; j1 = 0 ; i2 = 0 ; j2 = 0 ; rfact_zoom = 1. ; vcb = [0.02, 0.15, 0.4, 0.04] ; font_rat = 2./rfact_zoom
     x_cnf = 1200. ; y_cnf = 800. ; # where to put label of conf on Figure...
     l_show_cb = True ; l_show_nm = True ; l_scientific_mode=False
     bathy_max = 6000. # m
+
+elif CNEMO == 'GEBCO':
+    i1 = 0 ; j1 = 0 ; i2 = 0 ; j2 = 0 ; rfact_zoom = 1. ; vcb = [0.2, 0.06, 0.6, 0.03] ; font_rat = 1.5/rfact_zoom
+    x_cnf = 900. ; y_cnf = 1350. ; # where to put label of conf on Figure...
+    l_show_cb = True ; l_show_nm = True ; l_scientific_mode=False
+    bathy_max = 3000. # m
     
 elif CNEMO == 'Azores':
     i1 = 0 ; j1 = 0 ; i2 = 0 ; j2 = 0 ; rfact_zoom = 2. ; vcb = [0.15, 0.96, 0.8, 0.02] ; font_rat = 1./rfact_zoom*10.
@@ -185,7 +201,7 @@ if cv_in in ['sosstsst','tos']:
     cunit = r'SST ($^{\circ}$C)'
     cb_jump = 1
 
-elif cv_in in ['Bathymetry']:   # 
+elif cv_in in l_bathy_var:   # 
     cfield = 'Bathymetry'       # 
     tmin=0. ;  tmax=bathy_max   ;  df = 100.
     #cpal_fld = 'ocean'
@@ -240,10 +256,12 @@ if 'time_counter' in list_var:
     print '   => '+str(Nt)+' snapshots!'
     if jt <= 0 or jt > Nt: print ' PROBLEM: your time record does not exist!', jt ; sys.exit(0)
 else:
-    print '\n There is NO "time_counter" in file '+cf_fld+' !'
-    Nt = 0    
+    print '\nWARNING: there is NO "time_counter" in file '+cf_fld+' !'
+    print '   ==> setting Nt = 0 !\n'
+    Nt = 0
 id_fld.close()
 
+ibath=1
 
 if l_read_lsm:
     bt.chck4f(cf_lsm)
@@ -263,13 +281,19 @@ if l_read_lsm:
     id_lsm.close()
     print '      done.'
 
-elif cv_in == 'Bathymetry':
+elif cv_in in l_bathy_var:
     bt.chck4f(cf_fld)
     print '\n *** Will build mask from "Bathymetry"...'
     id_fld = Dataset(cf_fld)
+    list_dim = id_fld.dimensions.keys()
     nb_dim = len(id_fld.variables[cv_in].dimensions)
-    Ni = id_fld.dimensions['x'].size
-    Nj = id_fld.dimensions['y'].size
+    for jd in range(nb_dim):
+        if list_dim[jd] in ['x','lon','longitude','glamt']: cdim_x = list_dim[jd]
+        if list_dim[jd] in ['y','lat','latitude' ,'gphit']: cdim_y = list_dim[jd]
+    print ' *** x, y dims =>', cdim_x, cdim_y
+    #
+    Ni = id_fld.dimensions[cdim_x].size
+    Nj = id_fld.dimensions[cdim_y].size
     if i2 == 0: i2 = Ni
     if j2 == 0: j2 = Nj
     if nb_dim == 3: XBATH = id_fld.variables[cv_in][0,  j1:j2,i1:i2] ; # t, y, x
@@ -278,16 +302,29 @@ elif cv_in == 'Bathymetry':
         Xlon = id_fld.variables['nav_lon'][j1:j2,i1:i2]
         Xlat = id_fld.variables['nav_lat'][j1:j2,i1:i2]
     id_fld.close()
+
+    # Does bathymetry come as positive or negative in file???
+    bt_max = nmp.max(XBATH[:,:])
+    bt_min = nmp.min(XBATH[:,:])
+    print 'min, max bathy =>', bt_max, bt_min
+
+    if abs(bt_max) > 12000. or abs(bt_min) > 12000.:
+        print 'PROBLEM #1: we dont know what do do with min and max of bathymetry!'; sys.exit(0)
+    if abs(bt_min) > abs(bt_max):
+        print ' *** Bathymetry seems negative!'
+        ibath = -1
+        XBATH[:,:] = XBATH[:,:]*ibath
+    
     XMSK = nmp.zeros((Nj,Ni))
-    idx_oce = nmp.where(XBATH>0.2)
+    idx_oce = nmp.where(XBATH > 0.2)
     XMSK[idx_oce] = 1.
     del XBATH
     print '      done.'
 
     
 else:
-    print 'PROBLEM #1'; sys.exit(0)
-    
+    print 'PROBLEM #2'; sys.exit(0)
+
 
 print '\n According to "tmask" the shape of the domain is Ni, Nj =', Ni, Nj
 
@@ -387,8 +424,11 @@ if XMSK.shape != XFLD.shape:
 
 print '  *** Shape of field and mask => ', nmp.shape(XFLD)
 
-
 del XMSK
+
+
+if cv_in in l_bathy_var and ibath==-1: XFLD = ibath*XFLD
+
 
 (idy_nan,idx_nan) = nmp.where( nmp.isnan(XFLD) )
 
