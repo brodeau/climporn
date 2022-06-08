728c2f049bc735299fc9369d15e91b4c0eabd979
brodeau <brodeau@gmail.com>
2022-06-08 15:32:25 +0200

    LB

--- python/scripts/show_orca_grid_globe_from_coordinates.py
+++ python/scripts/show_orca_grid_globe_from_coordinates.py
@@ -39,18 +39,21 @@ if __name__ == '__main__':
 
     narg = len(sys.argv)
     if narg != 4:
-        print('Usage: '+sys.argv[0]+' <nemo_coordinates_file> <CONF_NAME> <Nsubsamp>'); sys.exit(0)
+        print('Usage: '+sys.argv[0]+' <nemo_coordinates_file> <cell_subsamp> <resolution (1,2,4,12,..,36,60)>'); sys.exit(0)
 
-    cf_mm    =     sys.argv[1]
-    conf     =     sys.argv[2]
-    isubsamp = int(sys.argv[3])
+    cf_coor  =     sys.argv[1]
+    isubsamp = int(sys.argv[2])
+    ires     = int(sys.argv[3])
 
-    #
-    cp.chck4f(cf_mm)
+    
+    cfig = str.replace(cf_coor,".nc","")
+    cfig = str.replace(cfig,"coordinates_","")
+    
+    cp.chck4f(cf_coor)
 
-    id_mm = Dataset(cf_mm)
+    id_coor = Dataset(cf_coor)
 
-    list_dim = id_mm.dimensions.keys()
+    list_dim = id_coor.dimensions.keys()
     l_td = 'time_counter' in list_dim or 'time' in list_dim
     l_zd = 'z' in list_dim
 
@@ -59,19 +62,19 @@ if __name__ == '__main__':
     if l_zd: nbdim = nbdim + 1
     #print(list_dim, l_td, l_zd)
     
-    Ni = id_mm.dimensions['x'].size
-    Nj = id_mm.dimensions['y'].size
+    Ni = id_coor.dimensions['x'].size
+    Nj = id_coor.dimensions['y'].size
 
     print('\n *** Horizontal domain => '+str(Ni)+' x '+str(Nj))
 
-    list_var = id_mm.variables.keys()
-    print('\n *** Variables in '+cf_mm+':\n', list(list_var), '\n')
+    list_var = id_coor.variables.keys()
+    print('\n *** Variables in '+cf_coor+':\n', list(list_var), '\n')
     list_treat = []
     list_ndims = []
     list_point = []
     for cv in list_var:
         if not cv in ['nav_lon','nav_lat','deptht','time_counter','mask']:
-            nbd = len(id_mm.variables[cv].dimensions)
+            nbd = len(id_coor.variables[cv].dimensions)
             if nbd > 1:
                 list_treat.append(cv)
                 list_ndims.append(nbd)
@@ -102,24 +105,24 @@ if __name__ == '__main__':
             # There is always time_counter as a dimmension
             if   xcv_d[jv] == nbdim:
                 if   nbdim==4:
-                    XVF[jv,:,:] = id_mm.variables[cv][0,0,:,:]
+                    XVF[jv,:,:] = id_coor.variables[cv][0,0,:,:]
                 elif nbdim==3:
-                    XVF[jv,:,:] = id_mm.variables[cv][0,:,:]
+                    XVF[jv,:,:] = id_coor.variables[cv][0,:,:]
                 else:
-                    XVF[jv,:,:] = id_mm.variables[cv][:,:]
+                    XVF[jv,:,:] = id_coor.variables[cv][:,:]
             else:
                 print(' PROBLEM: variable '+cv+' has '+str(xcv_d[jv])+' dimensions!!!'); sys.exit(0)
                
             jv=jv+1
 
-    id_mm.close()
+    id_coor.close()
 
     
     # Location of north pole:
     NNN = nmp.argmax( XVF[id_gphit,:,:] )
     NjNP = NNN // Ni
     NiNP = NNN % Ni    
-    print( "\n *** North Pole found at : Ni, Nj = ", NiNP, NjNP  )    
+    print( "\n *** North Pole found at : Ni, Nj = ", NiNP, NjNP, ' => ', XVF[id_gphit,NjNP,NiNP]  )    
     #cp.dump_2d_field( 'LAT.nc',  XVF[id_gphit,:,:] ) ; #debug
 
     #########################################
@@ -132,11 +135,11 @@ if __name__ == '__main__':
     
 
     # Small box including the North Pole to spot possible inconsistencies in the grid:
-    ii = cp.PlotGridGlobe( XVF[id_glamf,j1:j2,i1:i2], XVF[id_gphif,j1:j2,i1:i2], chemi='N', lon0=-35., cfig_name='zoom_NH_35W_f_OUT_ortho_WHITE.svg',
-                           nsubsamp=1, rdpi=DPIsvg, ldark=False, nzoom=5, linew=0.05 )
+    ii = cp.PlotGridGlobe( XVF[id_glamf,j1:j2,i1:i2], XVF[id_gphif,j1:j2,i1:i2], chemi='N', lon0=0., cfig_name='zoom_'+cfig+'_NH_35W_f_OUT_ortho_WHITE.png',
+                           nsubsamp=1, rdpi=DPIsvg, hres=1./float(ires), ldark=False, nzoom=1, linew=1., lNPzoom=True, l_show_lat_bg=True )
 
     # White:
-    ii = cp.PlotGridGlobe( XVF[id_glamf,:,:], XVF[id_gphif,:,:], chemi='N', lon0=-35., cfig_name='NH_35W_f_OUT_ortho_WHITE.svg',  nsubsamp=isubsamp, rdpi=DPIsvg, ldark=False )
-    ii = cp.PlotGridGlobe( XVF[id_glamf,:,:], XVF[id_gphif,:,:], chemi='N', lon0=130., cfig_name='NH_130E_f_OUT_ortho_WHITE.svg', nsubsamp=isubsamp, rdpi=DPIsvg, ldark=False )
+    ii = cp.PlotGridGlobe( XVF[id_glamf,:,:], XVF[id_gphif,:,:], chemi='N', lon0=-35., lat0=68., cfig_name=cfig+'_NH_35W_f_OUT_ortho_WHITE.svg',  nsubsamp=isubsamp, rdpi=DPIsvg, ldark=False )
+    ii = cp.PlotGridGlobe( XVF[id_glamf,:,:], XVF[id_gphif,:,:], chemi='N', lon0=130., lat0=68., cfig_name=cfig+'_NH_130E_f_OUT_ortho_WHITE.svg', nsubsamp=isubsamp, rdpi=DPIsvg, ldark=False )
 
 
