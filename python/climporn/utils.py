c6f239ec494e17f40eb0b4b0bc7bfe76cb3c3c2d
brodeau <brodeau@gmail.com>
2023-02-09 17:20:38 +0100

    LB

--- python/climporn/utils.py
+++ python/climporn/utils.py
@@ -825,10 +825,10 @@ def fig_style( pzoom, clr_top='k', clr_top_cb='k' ):
     fig_style.cfont_clb  = { 'fontname':'Open Sans',       'fontweight':'medium', 'fontsize':int(18.*pzoom), 'color':clr_top_cb }
     fig_style.cfont_date = { 'fontname':'Ubuntu Mono',     'fontweight':'normal', 'fontsize':int(12.*pzoom), 'color':clr_top }
     fig_style.cfont_mail = { 'fontname':'Times New Roman', 'fontweight':'normal', 'fontsize':int(14.*pzoom), 'color':'0.8' , 'fontstyle':'italic' }
-    fig_style.cfont_mrkr = { 'fontname':'Open Sans',       'fontweight':'light' , 'fontsize':int( 5.*pzoom), 'color':clr_top }
+    fig_style.cfont_mrkr = { 'fontname':'Open Sans',       'fontweight':'light' , 'fontsize':int( 8.*pzoom), 'color':clr_top }
     fig_style.cfont_axis = { 'fontname':'Open Sans',       'fontweight':'medium', 'fontsize':int(18.*pzoom), 'color':clr_top }
     fig_style.cfont_ttl  = { 'fontname':'Open Sans',       'fontweight':'medium', 'fontsize':int(32.*pzoom), 'color':clr_top }
-    fig_style.cfont_clck = { 'fontname':'Ubuntu Mono',     'fontweight':'normal', 'fontsize':int(18.*pzoom), 'color':clr_top }
+    fig_style.cfont_clck = { 'fontname':'Ubuntu Mono',     'fontweight':'normal', 'fontsize':int(13.*pzoom), 'color':clr_top }
     #
     return 0
 
