d8291f5792111948ecd706e48e0b7998be36d21d
brodeau <brodeau@gmail.com>
2024-03-22 11:47:14 +0100

    LB

--- python/climporn/field_aspect.py
+++ python/climporn/field_aspect.py
@@ -251,7 +251,7 @@ class field_aspect:
             cv_in = CWHAT  ; cv_out = cv_in ; color_top_cb='w'
             rmult = 3600.*24.
             #cpal_fld='inferno' ; tmin=0. ;  tmax=1.5 ; cb_extend = 'max'
-            cpal_fld='gray_r' ; tmin=0. ;  tmax=1.5 ; cb_extend = 'max' ; color_top_cb='k'; color_top='r'
+            cpal_fld='gray_r' ; tmin=0. ;  tmax=1.5 ; cb_extend = 'max' ; color_top_cb='k'; color_top='k'
             #cpal_fld='viridis' ; tmin=0. ;  tmax=1. ; cb_extend = 'max'
             #imask_no_ice_pc = 50 ; #color_missing = 'k' ; # we hide the field where A<`imask_no_ice_pc`%
             l_pow_field=True ; pow_field=0.3 ;  vc_fld_powlog= [tmin, 0.01, 0.1, 0.5, tmax ]
