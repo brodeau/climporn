46cf90c2db5655e8c276f9467d1e133b0289199d
brodeau <brodeau@gmail.com>
2024-05-21 11:02:53 +0200

    LB

--- python/scripts/nemo_movie_sclr.py
+++ python/scripts/nemo_movie_sclr.py
@@ -331,6 +331,7 @@ if l_add_SSU_to_ice_field:
     #cpal_ssu = 'cmocean_deep_r'
     cpal_ssu = 'cmocean_tempo'
     #cpal_ssu = 'cmocean_ice_r'
+    if CBOX in ["Spitzberg"]: cpal_ssu = 'cmocean_amp'
     rmin_ssu = 0. ; rmax_ssu = 1. ; dssu = 0.1
     pal_ssu = cp.chose_colmap(cpal_ssu)
     norm_ssu = colors.Normalize(vmin=rmin_ssu, vmax=rmax_ssu , clip = False)
