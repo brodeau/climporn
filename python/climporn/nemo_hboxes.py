8d65e9f7f505d057882557442c2cd6d3779611f2
brodeau <brodeau@gmail.com>
2024-05-21 10:39:25 +0200

    LB

--- python/climporn/nemo_hboxes.py
+++ python/climporn/nemo_hboxes.py
@@ -143,7 +143,7 @@ class nemo_hbox:
         elif [ config, box ] == [ 'NANUK36', 'Baffin']:
             i1=1160 ; j1=850 ; i2=i1+720 ; j2=j1+1200  ; rfact_zoom=1. ; font_rat=0.6*rfact_zoom
             l_show_cb  = True ; vcb=[0.2, 0.96, 0.6, 0.02]
-            l_show_clock=True ; x_clock = rfact_zoom*500 ; y_clock = rfact_zoom*1100
+            l_show_clock=True ; x_clock = rfact_zoom*500 ; y_clock = rfact_zoom*30
             l_show_sign=True  ; x_sign = rfact_zoom*540 ; y_sign = rfact_zoom*15
             pt_sz_track = 1
             #
