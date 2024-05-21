c43b03d5dc0aa095d974749842c800e6a612c8dc
brodeau <brodeau@gmail.com>
2024-05-21 12:47:04 +0200

    LB

--- python/climporn/field_aspect.py
+++ python/climporn/field_aspect.py
@@ -214,7 +214,7 @@ class field_aspect:
             cunit = 'Sea-ice damage'
             #
             print('LOLO: BOX = ', CBOX);exit(0)
-            if CBOX in ["CentralArctic"]: pal_fld='cmocean_gray_r'; pow_field=3.; color_top='k'; color_top_cb='k'
+            if CBOX=="CentralArctic": cpal_fld='cmocean_gray_r'; pow_field=3.; color_top='k'; color_top_cb='k'
 
 
             
