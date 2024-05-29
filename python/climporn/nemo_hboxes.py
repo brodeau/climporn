457fbf520b26950535acc3dcc157d20e65c39b37
brodeau <brodeau@gmail.com>
2024-05-29 18:40:04 +0200

    LB

--- python/climporn/nemo_hboxes.py
+++ python/climporn/nemo_hboxes.py
@@ -145,12 +145,11 @@ class nemo_hbox:
             ####   NANUK36   ####
         elif [ config, box ] == [ 'NANUK36', 'ALL']:
             i1=0 ; j1=54 ; i2=Ni0-20 ; j2=Nj0-100  ; rfact_zoom=0.3572 ; font_rat=2.*rfact_zoom ; # rfact_zoom=0.3572 => 1440p in height!
-            l_show_cb  = True ; vcb=[0.65, 0.965, 0.34, 0.016]
+            l_show_cb  = True ; vcb=[0.65, 0.965, 0.34, 0.016]; loc_cb = 'land'
             l_show_clock=True ; x_clock = rfact_zoom*3500 ; y_clock = rfact_zoom*3000
             l_show_sign=True  ; x_sign = rfact_zoom*3600 ; y_sign = rfact_zoom*40
             l_show_name=True  ; x_name  = rfact_zoom*100 ; y_name  = rfact_zoom*3700
             pt_sz_track = 1
-            loc_cb = 'land'
             #
         elif [ config, box ] == [ 'NANUK36', 'DEBUG']:
             i1=2575 ; j1=2073 ; i2=i1+480 ; j2=j1+480  ; rfact_zoom=3. ; font_rat=0.15*rfact_zoom
@@ -239,7 +238,8 @@ class nemo_hbox:
             ####   NANUK12   ####
         elif [ config, box ] == [ 'NANUK12', 'ALLs']:
             i1=0 ; j1=Nj0-Ni0+20  ;  i2=Ni0-35 ; j2=Nj0-15  ; rfact_zoom=1. ; font_rat=0.9*rfact_zoom
-            l_show_cb  = True ; vcb=[0.18, 0.075, 0.64, 0.02]
+            #l_show_cb  = True ; vcb=[0.18, 0.075, 0.64, 0.02]
+            l_show_cb  = True ; vcb=[0.65, 0.965, 0.34, 0.016]; loc_cb = 'land'
             l_show_clock=True ; x_clock = rfact_zoom*990 ; y_clock = rfact_zoom*1350
             l_show_name=True  ; x_name  = rfact_zoom*60 ; y_name  = rfact_zoom*1260
             l_show_exp=False  ; x_exp   = rfact_zoom*140 ; y_exp   = rfact_zoom*1250 
