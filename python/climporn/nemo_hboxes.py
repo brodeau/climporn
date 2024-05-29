af46ebc8df764ab96d087ebe58c4ad5a5d17370b
brodeau <brodeau@gmail.com>
2024-05-29 18:47:30 +0200

    LB

--- python/climporn/nemo_hboxes.py
+++ python/climporn/nemo_hboxes.py
@@ -239,8 +239,8 @@ class nemo_hbox:
         elif [ config, box ] == [ 'NANUK12', 'ALLs']:
             i1=0 ; j1=Nj0-Ni0+20  ;  i2=Ni0-35 ; j2=Nj0-15  ; rfact_zoom=1. ; font_rat=0.9*rfact_zoom
             #l_show_cb  = True ; vcb=[0.18, 0.075, 0.64, 0.02]
-            l_show_cb  = True ; vcb=[0.6, 0.965, 0.4, 0.016]; loc_cb = 'land'
-            l_show_clock=True ; x_clock = rfact_zoom*1100 ; y_clock = rfact_zoom*1150
+            l_show_cb  = True ; vcb=[0.55, 0.965, 0.4, 0.016]; loc_cb = 'land'
+            l_show_clock=True ; x_clock = rfact_zoom*1110 ; y_clock = rfact_zoom*1160
             l_show_name=True  ; x_name  = rfact_zoom*60 ; y_name  = rfact_zoom*1260
             l_show_exp=False  ; x_exp   = rfact_zoom*140 ; y_exp   = rfact_zoom*1250 
             l_add_logo=False  ; l_fill_holes_k=False
