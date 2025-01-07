8c520353d8ef7ea3973a1c663e11c643da787332
brodeau <brodeau@gmail.com>
2025-01-07 17:23:37 +0100

    LB

--- python/scripts/nemo_movie_sclr.py
+++ python/scripts/nemo_movie_sclr.py
@@ -341,6 +341,7 @@ if l_add_SSH_to_ice_field:
 # SSU over open ocean:
 if l_add_SSU_to_ice_field:
     cpal_ssu = 'cmocean_thermal'
+    if CWHAT=="qns_atmo": cpal_ssu = 'ice'
     rmin_ssu = 0. ; rmax_ssu = 1. ; dssu = 0.1
     pal_ssu = cp.chose_colmap(cpal_ssu)
     norm_ssu = colors.Normalize(vmin=rmin_ssu, vmax=rmax_ssu , clip = False)
