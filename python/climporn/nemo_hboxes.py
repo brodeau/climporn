8266a86529158188098767a92fc72422471385e1
brodeau <brodeau@login7.head.adastra.cines.fr>
2026-01-20 11:31:31 +0100

    LB

--- python/climporn/nemo_hboxes.py
+++ python/climporn/nemo_hboxes.py
@@ -227,12 +227,21 @@ class nemo_hbox:
 
             
             ####   NANUK12   ####
+        elif [ config, box ] == [ 'NANUK12', 'ALL']:
+            i1=0 ; j1=0  ;  i2=Ni0 ; j2=Nj0  ; rfact_zoom=1. ; font_rat=0.8*rfact_zoom
+            l_show_cb  = True ; vcb=[0.57, 0.965, 0.4, 0.016]; loc_cb = 'land'
+            l_show_clock=True ; x_clock = rfact_zoom*1180; y_clock = rfact_zoom*1460
+            l_show_name=False  ; x_name  = rfact_zoom*60 ; y_name  = rfact_zoom*1260
+            l_show_exp=False  ; x_exp   = rfact_zoom*140 ; y_exp   = rfact_zoom*1250 
+            l_add_logo=False  ; l_fill_holes_k=False
+            l_show_sign=True  ; x_sign = 1174*rfact_zoom ; y_sign = rfact_zoom*15
+            #
         elif [ config, box ] == [ 'NANUK12', 'ALLs']:
             i1=0 ; j1=Nj0-Ni0+20  ;  i2=Ni0-35 ; j2=Nj0-15  ; rfact_zoom=1. ; font_rat=0.8*rfact_zoom
             #l_show_cb  = True ; vcb=[0.18, 0.075, 0.64, 0.02]
             l_show_cb  = True ; vcb=[0.57, 0.965, 0.4, 0.016]; loc_cb = 'land'
             l_show_clock=True ; x_clock = rfact_zoom*1160 ; y_clock = rfact_zoom*1160
-            l_show_name=True  ; x_name  = rfact_zoom*60 ; y_name  = rfact_zoom*1260
+            l_show_name=False  ; x_name  = rfact_zoom*60 ; y_name  = rfact_zoom*1260
             l_show_exp=False  ; x_exp   = rfact_zoom*140 ; y_exp   = rfact_zoom*1250 
             l_add_logo=False  ; l_fill_holes_k=False
             l_show_sign=True  ; x_sign = 1174*rfact_zoom ; y_sign = rfact_zoom*15
@@ -260,7 +269,7 @@ class nemo_hbox:
             l_show_cb  = True ; vcb=[0.1, 0.1, 0.8, 0.02]
             l_show_clock=True ; x_clock = 310.*rfact_zoom ; y_clock = 460*rfact_zoom
             l_add_logo=False
-            l_show_name=True ; x_name = rfact_zoom*15 ; y_name = rfact_zoom*430
+            l_show_name=False ; x_name = rfact_zoom*15 ; y_name = rfact_zoom*430
             l_show_exp=False ; x_exp = 50 ; y_exp = 80
             l_fill_holes_k=False
             l_show_sign=True; x_sign = 398*rfact_zoom ; y_sign = 7*rfact_zoom
