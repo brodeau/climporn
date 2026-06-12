c1272a17ed14d7b23abaacab16130caa46dd91bc
brodeau <brodeau@gmail.com>
2026-06-12 17:15:23 +0200

    LB

--- python/scripts/show_orca_grid_globe_from_coordinates.py
+++ python/scripts/show_orca_grid_globe_from_coordinates.py
@@ -52,10 +52,10 @@ if __name__ == '__main__':
     ires     = int(sys.argv[3])
 
     if l_add_res_km: fl2d_mandat = np.concatenate([fl2d_mandat , ['e1t','e2t','tmask']])
-    
+
     cfig = path.basename( str.replace(cf_coor,".nc","") )
     cfig = str.replace(cfig,"coordinates_","")
-    
+
     cp.chck4f(cf_coor)
 
     id_coor = Dataset(cf_coor)
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     if l_td: nbdim = nbdim + 1
     if l_zd: nbdim = nbdim + 1
     #print(list_dim, l_td, l_zd)
-    
+
     Ni = id_coor.dimensions['x'].size
     Nj = id_coor.dimensions['y'].size
 
@@ -90,7 +90,7 @@ if __name__ == '__main__':
                 list_point.append(cc)
     nbv = len(list_treat)
     xcv_n = np.asarray(list_treat)
-    xcv_d = np.asarray(list_ndims)       
+    xcv_d = np.asarray(list_ndims)
     xcv_p = np.asarray(list_point)
 
 
@@ -99,9 +99,9 @@ if __name__ == '__main__':
     for cv in xcv_n:
 
         if cv in fl2d_mandat:
-        
+
             print('\n  ==> reading variable # '+str(jv+1)+' :'+cv+' ('+str(xcv_d[jv])+'D), grid point = '+xcv_p[jv])
-    
+
             if cv == 'glamf': id_glamf=jv
             if cv == 'gphif': id_gphif=jv
             if cv == 'glamt': id_glamt=jv
@@ -109,7 +109,7 @@ if __name__ == '__main__':
             if cv == 'e1t'  : id_e1t=jv
             if cv == 'e2t'  : id_e2t=jv
             if cv == 'tmask' : id_tmsk=jv
-    
+
             # There is always time_counter as a dimmension
             if   xcv_d[jv] == nbdim:
                 if   nbdim==4:
@@ -120,15 +120,15 @@ if __name__ == '__main__':
                     XVF[jv,:,:] = id_coor.variables[cv][:,:]
             else:
                 print(' PROBLEM: variable '+cv+' has '+str(xcv_d[jv])+' dimensions!!!'); sys.exit(0)
-               
+
             jv=jv+1
 
     id_coor.close()
 
-    
+
     # Location of north pole:
     (NjNP,NiNP) = np.unravel_index(XVF[id_gphit,:,:].argmax(), XVF[id_gphit,:,:].shape)
-    print( "\n *** North Pole found at : Ni, Nj = ", NiNP, NjNP, ' => ', XVF[id_gphit,NjNP,NiNP]  )    
+    print( "\n *** North Pole found at : Ni, Nj = ", NiNP, NjNP, ' => ', XVF[id_gphit,NjNP,NiNP]  )
     #cp.dump_2d_field( 'LAT.nc',  XVF[id_gphit,:,:] ) ; #debug
 
     #########################################
@@ -138,10 +138,10 @@ if __name__ == '__main__':
     # Zoom around North Pole:
     i1=NiNP-15 ; i2=NiNP+15
     j1=NjNP-15 ; j2=NjNP+15
-    
+
     # Small box including the North Pole to spot possible inconsistencies in the grid:
     ii = cp.PlotGridGlobe( XVF[id_glamf,j1:j2,i1:i2], XVF[id_gphif,j1:j2,i1:i2],
-                           Xglamt=XVF[id_glamt,j1:j2,i1:i2], Xgphit=XVF[id_gphit,j1:j2,i1:i2],                           
+                           Xglamt=XVF[id_glamt,j1:j2,i1:i2], Xgphit=XVF[id_gphit,j1:j2,i1:i2],
                            chemi='N', lon0=0., cfig_name='zoom_'+cfig+'_NH_35W_f_OUT_ortho_WHITE.png',
                            nsubsamp=1, rdpi=DPIsvg, hres=1./float(ires), ldark=False, nzoom=1, linew=1., lNPzoom=True )
 
@@ -149,19 +149,23 @@ if __name__ == '__main__':
     # White:
     if l_add_res_km:
         zshp = np.shape(XVF[id_glamf,:,:])
-        if zshp == (566, 492) or zshp == (4184, 4248) :
+        if zshp == (566, 492) or zshp == (1682, 1475) or zshp == (4184, 4248) :
             # nanuk*
             zmsk = np.ones( zshp, dtype='i1')
             zlon = np.mod( XVF[id_glamf,:,:], 360. )
             idx0 = np.where( (zlon>0.) & (zlon<100.) & (XVF[id_gphif,:,:]<48.) )   ; zmsk[idx0] = 0 ; # MedSea #1
             idx0 = np.where( (zlon<360.) & (zlon>355.) & (XVF[id_gphif,:,:]<43.) ) ; zmsk[idx0] = 0 ; # MedSea #2
             idx0 = np.where( (zlon>200.) & (zlon<230.) & (XVF[id_gphif,:,:]<62.) ) ; zmsk[idx0] = 0 ; # Alaska
+            rLat0 = 75.
             zfld = np.ma.masked_where( zmsk==0, XVF[id_e1t,:,:]/1000.)
             del zmsk, zlon, idx0
-            rLat0 = 75.
 
-        ii = cp.PlotGridGlobe( XVF[id_glamf,:,:], XVF[id_gphif,:,:], xField=zfld,
-                               chemi='N', lon0=-35., lat0=rLat0, cfig_name=cfig+'_NH_35W_f_OUT_ortho_WHITE.svg',  nsubsamp=isubsamp, rdpi=DPIsvg, ldark=False )
+            ii = cp.PlotGridGlobe( XVF[id_glamf,:,:], XVF[id_gphif,:,:], xField=zfld,
+                                   chemi='N', lon0=-35., lat0=rLat0, cfig_name=cfig+'_NH_35W_f_OUT_ortho_WHITE.svg',  nsubsamp=isubsamp, rdpi=DPIsvg, ldark=False )
+        else:
+            print(' Did not recognize the shape of you domain! :(')
+            exit(0)
+
         #ii = cp.PlotGridGlobe( XVF[id_glamf,:,:], XVF[id_gphif,:,:], xField=zfld,
         #                       chemi='N', lon0=130., lat0=rLat0, cfig_name=cfig+'_NH_130E_f_OUT_ortho_WHITE.svg', nsubsamp=isubsamp, rdpi=DPIsvg, ldark=False )
     else:
